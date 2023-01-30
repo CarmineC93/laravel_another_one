e3d25b894e0ff87654b6d5dd53dde828e51075e5
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-30 19:24:31 +0100

    tring to add auth verification

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-    <h1>POKEDEX</h1>
+    <h1>POKEMON</h1>
 
     <a class="btn btn-success text-light" href="{{ route('pokemons.index') }}">
-        POKEMON
+        POKEDEX
     </a>
 
 @endsection
