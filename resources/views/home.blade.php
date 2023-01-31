e53bf314f5ec953d88bee11b6579fdbe2c5e5f41
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-31 18:58:29 +0100

    add pic in file home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,16 @@
 
 @section('content')
 
-    <h1>POKEMON</h1>
+    <div class="container bg-dark">
+        <div class="m-4">
+            <img class="d-block m-2" src="{{ Vite::asset('/resources/img/logo.png') }}" alt="pokedex">
 
-    <a class="btn btn-success text-light" href="{{ route('pokemons.index') }}">
-        POKEDEX
-    </a>
+            <a class="btn btn-light text-success my-2" href="{{ route('pokemons.index') }}">
+                POKEDEX
+                <img src="{{ Vite::asset('/resources/img/pokedex.png') }}" alt="pokedex">
+            </a>
+
+        </div>
+    </div>
 
 @endsection
