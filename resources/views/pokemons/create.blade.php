cc861ec6ddb9645e92dce700808f7c4d925b7e43
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-02-02 19:38:37 +0100

    start to fill create page

--- resources/views/pokemons/create.blade.php
+++ resources/views/pokemons/create.blade.php
@@ -1,1 +1,92 @@
+@extends('layouts.app')
 
+@section('content')
+    <div class="container">
+        <h2 class="text-center mt-3 text-light">Crea un nuovo Pokèmon</h2>
+        <div class="row justify-content-center">
+            <div class="col-8">
+
+
+                <form action="{{ route('pokemons.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group mb-3">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name"
+                            class="form-control @error('name')
+                        is-invalid
+                        @enderror"
+                            value="{{ old('name') }}">
+                        @error('name')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    {{-- //aggiungere tutti i campi per la creazione --}}
+
+                    {{-- 
+                    <div class="form-group mb-3">
+                        <h4>technologies</h4>
+                        @foreach ($technologies as $technology)
+                            <div class="form-check"> --}}
+                    {{-- 'value' deve contenere i'id da salvare che alla selezione del checkbox viene salvato tramite il 'name' nell'array collection [technologies], array che è nella tabella ponte project-technology (non nella tabella posts!) --}}
+                    {{-- <input type="checkbox" name="technologies[]" id="technology-{{ $technology->id }}"
+                                    class="form-check-input" value="{{ $technology->id }}">
+                                <label for="technology-{{ $technology->id }}"
+                                    class="form-check-label">{{ $technology->name }}</label>
+                            </div>
+                        @endforeach
+
+                    </div> --}}
+                    {{-- 
+                    <div class="form-group mb-3">
+                        <label for="type">Categoria</label> --}}
+                    {{-- il 'name' del select deve essere come il nome della colonna --}}
+                    {{-- <select name="type_id" id="type" class="form-select">
+                            <option value="">Nessun tipo</option>
+                            @foreach ($types as $type)
+                                <option value="{{ $type->id }}" @selected(old('type_id') == $type->id)>{{ $type->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="cover_image">Immagine</label>
+                        <input type="file" name="cover_image" id="cover_image"
+                            class="form-control
+                            @error('cover_image')
+                            is-invalid
+                            @enderror">
+                        @error('cover_image')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div> --}}
+
+                    {{-- anteprima immagine che si aggiorna tramit attributo id colllegato ad app.js --}}
+                    {{-- <div class="mt-3">
+                        <img id="image_preview" src="" alt="" style="max-height: 200px">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="content">Descrizione</label>
+                        <textarea name="content" id="content" rows="10"
+                            class="form-control @error('content')
+                        is-invalid
+                        @enderror">{{ old('content') }}</textarea>
+                        @error('content')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div> --}}
+
+                    <button class="btn btn-success" type="submit">Salva</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
